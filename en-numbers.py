6a889814b55b406d7459304448de41f82d5acf63
Porter Liu <liuzhf@gmail.com>
2017-04-30 22:09:55 -0700

    add 'correct rate'

--- en-numbers.py
+++ en-numbers.py
@@ -20,6 +20,9 @@ if len( sys.argv ) > 1:
 
 print 'We will work on ' + str( min ) + ' ~ ' + str( max )
 
+amount  = 0
+correct = 0
+
 while True:
 	command = ['say', '-r', '240']
 	number = random.randint( min, max )
@@ -31,12 +34,16 @@ while True:
 	if len( answer ) == 0 or not answer.isdigit():
 		break
 
+	amount += 1
 	if int( answer ) == number:
 		subprocess.call( ['say', 'great'] )
+		correct += 1
 	else:
 		print "it's actually " + str( number )
 		subprocess.call( ['say', 'wrong'] )
 
+	print correct, amount
 	time.sleep( 0.5 )
 
+print 'correct rate = ' + str( int( float( correct ) / amount * 100.0 ) ) + '%'
 print 'bye'
